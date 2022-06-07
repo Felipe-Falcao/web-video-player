809ed75e843cc6f7d6df260ae60b69d3daca32d5
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2022-06-07 18:55:17 -0300

    style: action controls

--- src/pages/player/index.jsx
+++ src/pages/player/index.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import './style.css'
+import { FaVolumeMute, FaVolumeUp, FaPlay, FaPause } from 'react-icons/fa'
 import useVideoPlayer from '../../hooks/useVideoPlayer'
 
 const Player = () => {
@@ -17,49 +18,55 @@ const Player = () => {
     } = useVideoPlayer(videoElement)
 
     return (
-        <div className="container">
-            <input type="file" onChange={handleVideoUpload} />
-            <div className="video-wrapper">
+        <div className='container'>
+            <input type='file' onChange={handleVideoUpload} />
+            <div className='video-wrapper'>
                 <video
                     height={500}
                     src={videoFilePath}
                     ref={videoElement}
                     onTimeUpdate={handleOnTimeUpdate}
                 />
-                <div className="controls">
-                    <div className="actions">
-                        <button onClick={togglePlay}>
-                            {!playerState.isPlaying ? (
-                                'Play'
-                            ) : (
-                                'Pause'
-                            )}
-                        </button>
+                <div className='controls'>
+                    <div className='range-container'>
+                        <input
+                            type='range'
+                            min='0'
+                            max='100'
+                            value={playerState.progress}
+                            onChange={(e) => handleVideoProgress(e)}
+                        />
                     </div>
-                    <input
-                        type="range"
-                        min="0"
-                        max="100"
-                        value={playerState.progress}
-                        onChange={(e) => handleVideoProgress(e)}
-                    />
-                    <select
-                        className="velocity"
-                        value={playerState.speed}
-                        onChange={(e) => handleVideoSpeed(e)}
-                    >
-                        <option value="0.50">0.50x</option>
-                        <option value="1">1x</option>
-                        <option value="1.25">1.25x</option>
-                        <option value="2">2x</option>
-                    </select>
-                    <button className="mute-btn" onClick={toggleMute}>
-                        {!playerState.isMuted ? (
-                            'Mutar'
-                        ) : (
-                            'Desmutar'
-                        )}
-                    </button>
+                    <div className='actions-container'>
+                        <div className='left'>
+                            <button onClick={togglePlay} className='action-buttons'>
+                                {!playerState.isPlaying ? (
+                                    <FaPlay />
+                                ) : (
+                                    <FaPause />
+                                )}
+                            </button>
+                        </div>
+                        <div className='right'>
+                            <button onClick={toggleMute} className='action-buttons'>
+                                {!playerState.isMuted ? (
+                                    <FaVolumeUp />
+                                ) : (
+                                    <FaVolumeMute />
+                                )}
+                            </button>
+                            <select
+                                className='velocity'
+                                value={playerState.speed}
+                                onChange={(e) => handleVideoSpeed(e)}
+                            >
+                                <option value='0.50'>0.50x</option>
+                                <option value='1'>1x</option>
+                                <option value='1.25'>1.25x</option>
+                                <option value='2'>2x</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
